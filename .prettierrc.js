12141506300fe83ba499de1b746bf80fd740e0c4
Max Metral <max@pyralis.com>
2022-01-06 15:04:33 -0500

    Update SVGR/SVGO

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   bracketSpacing: true,
-  jsxBracketSameLine: true,
+  bracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   // A person has his limits, and 80 is not it.
